e8272a5a33f18823905ffc9d10f0951eec9aee6d
DEIDADEV <161657805+DEIDADEV@users.noreply.github.com>
2024-05-16 14:04:14 -0300

    salvando projeto CRUD - 16/05/2024

---
+++ crud-em-java/src/br/com/agenda/aplicacao/Main.java
@@ -0,0 +1,49 @@
+package br.com.agenda.aplicacao;
+
+import java.util.Date;
+
+import br.com.agenda.dao.ContatoDAO;
+import br.com.agenda.model.Contato;
+
+public class Main {
+	
+	/*
+	 * CRUD
+	 * C: CREATE
+	 * R: READ
+	 * U: UPDATE
+	 * D: DELETE
+	 */
+
+	public static void main(String[] args) {
+		
+		// C: CREATE = Cria e salva o contato no banco.
+		ContatoDAO contatoDAO = new ContatoDAO();
+		
+		Contato contato = new Contato();
+		contato.setNome("João Carlos Dias");
+		contato.setIdade(85);
+		contato.setDataCadastro(new Date());
+		
+		//contatoDAO.save(contato);
+		
+		// R: READ = Visualiza os registros do banco de dados (TODOS OS REGISTROS).
+		for(Contato c : contatoDAO.getContatos())
+			System.out.println("Contato: " + c.getNome());
+		
+		// U: UPDATE = Atualiza o contato no banco.
+		Contato c1 = new Contato();
+		c1.setNome("Pedro dias Orlando");
+		c1.setIdade(37);
+		c1.setDataCadastro(new Date());
+		c1.setId(10);//è o número que está no banco de dados da PK
+		
+		//contatoDAO.update(c1);
+		
+		// D: DELETE = Deleta o contato no banco, pelo número de ID.
+		contatoDAO.deleteByID(10);//è o número que está no banco de dados da PK
+		
+
+	}
+
+}
---
+++ crud-em-java/src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,42 @@
+package br.com.agenda.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+	
+	//Nome do usuário do MySQL
+	private static final String USERNAME = "root";
+	
+	//Senha do banco
+	private static final String PASSSWORD = "";
+	
+	//Caminho do banco de dados, (porta e nome do banco de dados)
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
+	
+	/*
+	 * Conexão com o banco de dados
+	 * */
+	public static Connection createConnectionToMySQL( ) throws Exception {
+		//Faz com que a classe seja carregada pela JVM
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		//Cria a conexão com o banco de dados
+		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSSWORD);
+		
+		return connection;
+	}
+	
+		public static void main(String[] args) throws Exception {
+			
+			//Recuperar uma conexão com o banco de dados
+			Connection con = createConnectionToMySQL();
+			
+			//Testar se a conexão é nula
+			if(con!=null) {
+				System.err.println("Conexão obtida com sucesso!");
+				con.close();
+			}
+		}
+	
+}
